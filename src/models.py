f9ead2cfdd262c42c7be759d0411b13dbe9bc46d
Ernesto Martinez <61639551+ErnestWarhead@users.noreply.github.com>
2024-04-26 17:20:00 +0000

    finished

--- src/models.py
+++ src/models.py
@@ -1,32 +1,51 @@
+from __future__ import annotations
+
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
-from sqlalchemy.orm import relationship, declarative_base
-from sqlalchemy import create_engine
+from sqlalchemy import Column, ForeignKey, Integer, String, create_engine
+from sqlalchemy.orm import relationship, declarative_base, Mapped
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class UserPlanetFavorite(Base):
+    __tablename__ = "user_planet_favorites"
+
+    user_id = Column(Integer, ForeignKey("users.id"), primary_key=True)
+    planet_id = Column(Integer, ForeignKey("planets.id"), primary_key=True)
+
+class UserCharacterFavorite(Base):
+    __tablename__ = "user_character_favorites"
+
+    user_id = Column(Integer, ForeignKey("users.id"), primary_key=True)
+    character_id = Column(Integer, ForeignKey("characters.id"), primary_key=True)
+
+class User(Base):
+    __tablename__ = "users"
+
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    email = Column(String, nullable=False)
+    password = Column(String, nullable=False)
+    favorite_planets: Mapped[list['Planet']] = relationship("Planet", secondary="user_planet_favorites")
+    favorite_characters: Mapped[list['Character']] = relationship("Character", secondary="user_character_favorites")
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Planet(Base):
+    __tablename__ = "planets"
+
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String, nullable=False)
+    population = Column(Integer, nullable=False)
+    terrain = Column(String, nullable=False)
+    favorited_by: Mapped[list['User']] = relationship("User", secondary="user_planet_favorites")
 
-    def to_dict(self):
-        return {}
+class Character(Base):
+    __tablename__ = "characters"
 
-## Draw from SQLAlchemy base
-render_er(Base, 'diagram.png')
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    hair_color = Column(String, nullable=False)
+    eye_color = Column(String, nullable=False)
+    gender = Column(String, nullable=False)
+    favorited_by: Mapped[list['User']] = relationship("User", secondary="user_character_favorites")
+
+render_er(Base, "diagram.png")
